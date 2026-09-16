a90db68573a74c5e7190fdf8881a071c80dc5b6b
kvichans <kvichans@mail.ru>
2026-09-16 14:55:02 +0300

    Create join-text_ЭффМанип.py

---
+++ join-text_ЭффМанип.py
@@ -0,0 +1,14 @@
+from timeit import timeit
+наб = ['1']*10_000
+def наи():
+  отв = ''
+  for сим in наб:
+    отв += сим
+  return отв
+def эфф():
+  return ''.join(наб)
+print(наи()==эфф()) # True
+вр_наи = timeit('наи()', globals=globals(), number=1000)
+вр_эфф = timeit('эфф()', globals=globals(), number=1000)
+print(вр_наи // вр_эфф)
+
